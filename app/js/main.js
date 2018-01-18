6e683da86465684e60a220396222f4993910e4c2
Alex-Shkuropatov <a.shkuropatov@quarnuts.com>
2018-01-19 00:57:11 +0200

    desktop main page fixes, contact page, articles page, catalog page done

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,14 @@
 $( document ).ready( function(){
 
+	/* calculate scrollbar width */
+	function getScrollBarWidth () {
+	    var $outer = $('<div>').css({visibility: 'hidden', width: 100, overflow: 'scroll'}).appendTo('body'),
+	        widthWithScroll = $('<div>').css({width: '100%'}).appendTo($outer).outerWidth();
+	    $outer.remove();
+	    return 100 - widthWithScroll;
+	};
+
+
 	/* show more button on brand slider on main page*/
 	$( '.brand-show-more' ).click( function(){
 		$( this ).prev().height( 'auto' );
@@ -10,12 +19,14 @@ $( document ).ready( function(){
 	$( '.brand-description-slider' ).slick({
 		arrows: false,
 		fade: true,
-		draggable: false
+		draggable: false,
+		accessibility: false
 	});
 	$( '.brand-preview-slider' ).slick({
 		slidesToShow: 5,
 		asNavFor: '.brand-description-slider',
 		swipeToSlide: true,
+		focusOnSelect: true,
 		nextArrow: '<div class="slider-arrow slider-arrow-next"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 512.035 512.035" fill="#eb7d93"><path d="M506.817,248.355L11.883,0.888C8.47-0.819,5.057,0.035,2.497,2.595c-2.56,3.413-3.413,7.68-0.853,10.24l158.72,243.2l-158.72,243.2c-1.707,3.413-1.707,7.68,0.853,10.24c0.853,1.707,3.413,2.56,5.973,2.56c1.707,0,2.56,0,3.413-0.853l494.933-247.467c3.413-1.707,5.12-4.267,5.12-7.68S509.377,250.061,506.817,248.355z M33.217,482.168l145.067-221.013c1.707-2.56,1.707-6.827,0-9.387L33.217,30.755l451.413,225.28L33.217,482.168z"/></svg></div>',
 		prevArrow: '<div class="slider-arrow slider-arrow-prev"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 512.035 512.035" fill="#eb7d93"><path d="M506.817,248.355L11.883,0.888C8.47-0.819,5.057,0.035,2.497,2.595c-2.56,3.413-3.413,7.68-0.853,10.24l158.72,243.2l-158.72,243.2c-1.707,3.413-1.707,7.68,0.853,10.24c0.853,1.707,3.413,2.56,5.973,2.56c1.707,0,2.56,0,3.413-0.853l494.933-247.467c3.413-1.707,5.12-4.267,5.12-7.68S509.377,250.061,506.817,248.355z M33.217,482.168l145.067-221.013c1.707-2.56,1.707-6.827,0-9.387L33.217,30.755l451.413,225.28L33.217,482.168z"/></svg></div>'
 	});
@@ -35,4 +46,84 @@ $( document ).ready( function(){
 		nextArrow: '<div class="slider-arrow slider-arrow-next"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 512.035 512.035" fill="#eb7d93"><path d="M506.817,248.355L11.883,0.888C8.47-0.819,5.057,0.035,2.497,2.595c-2.56,3.413-3.413,7.68-0.853,10.24l158.72,243.2l-158.72,243.2c-1.707,3.413-1.707,7.68,0.853,10.24c0.853,1.707,3.413,2.56,5.973,2.56c1.707,0,2.56,0,3.413-0.853l494.933-247.467c3.413-1.707,5.12-4.267,5.12-7.68S509.377,250.061,506.817,248.355z M33.217,482.168l145.067-221.013c1.707-2.56,1.707-6.827,0-9.387L33.217,30.755l451.413,225.28L33.217,482.168z"/></svg></div>',
 		prevArrow: '<div class="slider-arrow slider-arrow-prev"><svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 512.035 512.035" fill="#eb7d93"><path d="M506.817,248.355L11.883,0.888C8.47-0.819,5.057,0.035,2.497,2.595c-2.56,3.413-3.413,7.68-0.853,10.24l158.72,243.2l-158.72,243.2c-1.707,3.413-1.707,7.68,0.853,10.24c0.853,1.707,3.413,2.56,5.973,2.56c1.707,0,2.56,0,3.413-0.853l494.933-247.467c3.413-1.707,5.12-4.267,5.12-7.68S509.377,250.061,506.817,248.355z M33.217,482.168l145.067-221.013c1.707-2.56,1.707-6.827,0-9.387L33.217,30.755l451.413,225.28L33.217,482.168z"/></svg></div>'
 	});
+
+	$('.open-call-popup').magnificPopup({
+		items: {
+		    src: '#call-popup',
+		    type: 'inline'
+		},
+		mainClass: 'mfp-fade',
+		removalDelay: 250
+	});
+	//console.log(getScrollBarWidth());
+
+	/* desktop dropdown in header */
+	$( '.header-drop-down' ).parent().click(function( e ){
+		$(this).children('.header-drop-down').slideToggle();
+		e.preventDefault();
+	});
+
+	/* custom scrollbar in filters in catalog */
+	$(".scroll").mCustomScrollbar({
+		theme:"dark"
+	});
+
+	/* DROPDOWN FILTERS */
+	$( '.filter-name-wrap' ).click(function(){
+		$( this ).toggleClass( 'filter-open' );
+		var filterContent = $( this ).parent().children( '.filter-content-wrap' );
+		filterContent.slideToggle();
+	});
+
+	/* OWN CUSTOM SELECT */
+	$('.select-hidden').each(function(){
+	    var $this = $(this); 
+	    numberOfOptions = $(this).children('option').length;
+
+	    //var selectWidth = $this.width();
+
+
+	    $this.wrap('<div class="select-styled-wrap"></div>');
+	    $this.after('<div class="select-styled"></div>');
+
+	    var $styledSelect = $this.next('div.select-styled');
+	    $styledSelect.outerWidth( $this.outerWidth() );
+	    $styledSelect.text($this.children('option').eq(0).text());
+	  
+	    var $list = $('<ul />', {
+	        'class': 'select-options'
+	    }).insertAfter($styledSelect);
+	  
+	    for (var i = 0; i < numberOfOptions; i++) {
+	        $('<li />', {
+	            text: $this.children('option').eq(i).text(),
+	            rel: $this.children('option').eq(i).val()
+	        }).appendTo($list);
+	    }
+	  
+	    var $listItems = $list.children('li');
+	  
+	    $styledSelect.click(function(e) {
+	        e.stopPropagation();
+	        $('div.select-styled.select-open').not(this).each(function(){
+	            $(this).removeClass('select-open').next('ul.select-options').hide();
+	        });
+	        $(this).toggleClass('select-open').next('ul.select-options').toggle();
+	    });
+	  
+	    $listItems.click(function(e) {
+	        e.stopPropagation();
+	        $styledSelect.text($(this).text()).removeClass('select-open');
+	        $this.val($(this).attr('rel'));
+	        $list.hide();
+	        //console.log($this.val());
+	    });
+	  
+	    $(document).click(function() {
+	        $styledSelect.removeClass('select-open');
+	        $list.hide();
+	    });
+
+	});
+
 });
